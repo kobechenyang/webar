0ada5197f744c65840a8e506130f42ecf2f0c45b
yang.chen <yang.chen@crowdstar.com>
2019-07-16 17:14:17 +0800

    Updates

---
+++ precache-manifest.06de3af4c6238f4388d1ae44b7195d25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe4857979ba00d567e52a07a914b76b",
+    "url": "/webar/index.html"
+  },
+  {
+    "revision": "c2fb0d347c0a018ad1a2",
+    "url": "/webar/static/css/main.ce9a3cd1.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/webar/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "c2fb0d347c0a018ad1a2",
+    "url": "/webar/static/js/main.ad83200f.chunk.js"
+  },
+  {
+    "revision": "cd658cbf4b22c3362ada",
+    "url": "/webar/static/js/runtime~main.2894b0e4.js"
+  }
+]);
