26962811d2bbe75eb3ee90f85a0133acb94c2068
yang.chen <yang.chen@crowdstar.com>
2019-08-30 14:04:52 +0800

    Test pinch

--- public/script/main.js
+++ public/script/main.js
@@ -25,13 +25,13 @@ var mc = new Hammer.Manager(slidecontainer);
 // mc.get("pan").set({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 2 });
 mc.on("pinch", function (ev) {
     console.log(ev.scale);
-    const model = markerGroup.getObjectByName("model");
-    if(markerGroup.object3D.visible && model){
-        const size = Math.clamp(1, 10, 100*model.scale.x*ev.scale);
-        console.log(size + " , " + e.scale);
-        model.scale.set(0.01*size, 0.01*size, 0.01*size);
-        setDirectionligthSize(size);
-    }
+    // const model = markerGroup.getObjectByName("model");
+    // if(markerGroup.object3D.visible && model){
+    //     const size = Math.clamp(1, 10, 100*model.scale.x*ev.scale);
+    //     console.log(size + " , " + e.scale);
+    //     model.scale.set(0.01*size, 0.01*size, 0.01*size);
+    //     setDirectionligthSize(size);
+    // }
 });
 
 // mc.on( "pinchend", function( e ) {
